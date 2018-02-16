192eabd273d510e7e57cbfb10f1308ef11493a92
BeastHook <ukrowd@yahoo.de>
2018-02-16 13:27:42 +0100

    Menu 1.4.2
    + Setting: Sound, Music, Contol mit slidern und Toggle

---
+++ Space-Shooter/Assets/Scripts/Menu/ShowSliderValue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowSliderValue : MonoBehaviour {
+
+    private TextMeshProUGUI valueText;
+
+	// Use this for initialization
+	void Start () {
+        valueText = GetComponent<TextMeshProUGUI> ();
+    }
+	
+	// Update is called once per frame
+	public void textUpdate (float value) {
+        valueText.text = value.ToString();
+        
+	}
+}
