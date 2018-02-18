b183102a343562f2dd23909c0d2c995db0416de4
BeastHook <ukrowd@yahoo.de>
2018-02-18 16:20:31 +0100

    BlackHole

---
+++ Space-Shooter/Assets/Scripts/Power-Ups/BlackHoleBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleBehaviour : MonoBehaviour
+{
+    // Variables
+    private GameObject[] enemy;
+    private PowerUpSpawner spawnScript;
+    private Vector3 velocity = Vector3.zero;
+    private float powerUpTime = 10;
+
+    private void Start()
+    {
+        spawnScript = FindObjectOfType<PowerUpSpawner>().GetComponent<PowerUpSpawner>();
+    }
+
+    private void Update()
+    {
+        powerUpTime -= Time.deltaTime;
+        if (powerUpTime <= 0)
+        {
+            spawnScript.stop = false;
+            Destroy(gameObject);
+        }
+
+        enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemy.Length; i++)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy[i].transform.position);
+
+            if (distanceToEnemy <= 5)
+            {
+                enemy[i].transform.position = Vector3.SmoothDamp(enemy[i].transform.position, transform.position, ref velocity, 0.2f);
+                   // Vector3.MoveTowards(enemy[i].transform.position, transform.position, 0.3f);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Space-Shooter/Assets/Scripts/Power-Ups/BlackHolePowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHolePowerUp : MonoBehaviour {
+
+    // Variables
+    public GameObject pickUpEffect;
+    public GameObject blackHole;
+
+    private PowerUpSpawner spawnScript;
+    private bool powerUpActivated = false;
+    private float powerUpTime = 10;
+
+    private void Start()
+    {
+        spawnScript = FindObjectOfType<PowerUpSpawner>().GetComponent<PowerUpSpawner>();
+    }
+
+    // Spawn black hole if player is picking up
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Instantiate(pickUpEffect, transform.position, transform.rotation);
+            Instantiate(blackHole, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
