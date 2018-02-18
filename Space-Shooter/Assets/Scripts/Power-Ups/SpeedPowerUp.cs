5ef802b28c3725a342b1b33482864ad978a30b36
Xolution13 <e-lewandowski@web.de>
2018-02-18 13:41:50 +0100

    Some clean up to power up

--- Space-Shooter/Assets/Scripts/Power-Ups/SpeedPowerUp.cs
+++ Space-Shooter/Assets/Scripts/Power-Ups/SpeedPowerUp.cs
@@ -2,55 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedPowerUp : MonoBehaviour {
-
+public class SpeedPowerUp : MonoBehaviour
+{
     // Variables
-	public GameObject explosionAnimation;
-	private GameObject player;
-	private AccelerometerTest playerScript;
-	private PowerUpSpawner spawner;
-	private bool activated = false;
-	private bool doOnce = true;
-
+	public GameObject pickUpEffect;
+	private AccelerometerMovement player;
+	private AccelerometerMovement playerScript;
+	private PowerUpSpawner spawnScript;
+	private bool powerUpActivated = false;
 	private float powerUpTime = 10;
 
-	private void Awake(){
-	
-		player = GameObject.FindGameObjectWithTag ("Player");
-		playerScript = player.GetComponent<AccelerometerTest>();
-		spawner = GameObject.FindGameObjectWithTag("PowerUpSpawner").GetComponent<PowerUpSpawner>();
-
+	private void Start()
+    {
+		player = FindObjectOfType<AccelerometerMovement>();
+		playerScript = player.GetComponent<AccelerometerMovement>();
+        spawnScript = FindObjectOfType<PowerUpSpawner>().GetComponent<PowerUpSpawner>();
 	}
 
-	private void Update(){
-	
-		if (activated) {
-			GetComponent<Renderer> ().enabled = false;
-			playerScript.movementSpeed = 80;
-			if (doOnce) {
-				Instantiate (explosionAnimation, this.transform.position, this.transform.rotation);
-				doOnce = false;
-			}
-
+    // Reset power-up effect after time, destroy power-up and allow next spawn
+    private void Update()
+    {
+		if (powerUpActivated)
+        {
 			powerUpTime -= Time.deltaTime;
-			if (powerUpTime <= 1) {
+			if (powerUpTime <= 0)
+            {
 				Debug.Log ("Speed reset");
 				playerScript.movementSpeed = 50;
-				spawner.stop = false;
-				Destroy(this.gameObject);
+                spawnScript.stop = false;
+				Destroy(gameObject);
 			}
 		}
-
 	}
 
-
-	private void OnTriggerEnter(Collider objectToCollide){
-
-		if (objectToCollide.gameObject == player) {
-
-			activated = true;
+    // Check if the player is picking up
+	private void OnTriggerEnter(Collider other)
+    {
+		if (other.gameObject.tag == "Player")
+        {
+			powerUpActivated = true;
+            PickUp();
 		}
-
 	}
-		
+
+    // Instantiate pick-up effect and enable power-up effect
+    private void PickUp()
+    {
+        //Instantiate(pickUpEffect, transform.position, transform.rotation);
+        Debug.Log("Power-Up picked up!");
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        playerScript.movementSpeed = 80;
+    }
 }
