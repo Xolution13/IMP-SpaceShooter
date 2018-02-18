a9735fc012cf0f0747cab1e8ca5e4695563949a8
Xolution13 <e-lewandowski@web.de>
2018-02-18 11:01:57 +0100

    Finished Dancer Script
    Danacer Script is done

--- Space-Shooter/Assets/Scripts/Enemies/DancerBehaviour.cs
+++ Space-Shooter/Assets/Scripts/Enemies/DancerBehaviour.cs
@@ -8,13 +8,13 @@ public class DancerBehaviour : MonoBehaviour
     private GameObject[] bullet;
     private AccelerometerMovement player;
     private Vector3 velocity = Vector3.zero;
-    public float distanceToBullet;
+    private float distanceToBullet;
     public float moveSpeed = 3;
     public float dodgeSpeed = 4;
-    public int randomNumber;
-    public string direction;
-    public bool isChasingPlayer = true;
-    public bool isDodgingBullet = false;
+    private int randomNumber;
+    private bool inBulletLine = false;
+    private bool isChasingPlayer = true;
+    private bool isDodgingBullet = false;
 
     private void Start()
     {
@@ -25,39 +25,64 @@ public class DancerBehaviour : MonoBehaviour
 
     private void Update()
     {
+        // Update bullets as they spawn
         bullet = GameObject.FindGameObjectsWithTag("Bullet");
+
+        // Limit position and rotation (so we do not have to use colliders)
         transform.position = new Vector3((Mathf.Clamp(transform.position.x, -24, 24)),
                                             transform.position.y,
                                             Mathf.Clamp(transform.position.z, -16.5f, 16.5f));
+        transform.rotation = Quaternion.Euler(new Vector3(0, transform.eulerAngles.y, 0));
 
-        if (isChasingPlayer && !isDodgingBullet)
+        // If the enemy is not in front of player and the player is not shooting
+        if (isChasingPlayer && !isDodgingBullet && !inBulletLine)
         {
             transform.LookAt(player.transform.position);
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
 
+        // Calculate distance to every bullet and dodge it if enemy is in front of player
         for (int i = 0; i < bullet.Length; i++)
         {
             distanceToBullet = Vector3.Distance(transform.position, bullet[i].transform.position);
 
-            if (distanceToBullet <= 10)
+            if (inBulletLine)
             {
-                isDodgingBullet = true;
-                transform.LookAt(Vector3.SmoothDamp(transform.position, bullet[i].transform.position, ref velocity, 0.5f));
+                if (distanceToBullet <= 10)
+                {
+                    isDodgingBullet = true;
+                    transform.LookAt(Vector3.SmoothDamp(transform.position, bullet[i].transform.position, ref velocity, 0.5f));
 
-                if (randomNumber == 0)
-                {
-                    transform.Translate((Vector3.back + Vector3.right) * dodgeSpeed * Time.deltaTime);
-                }
-                else if (randomNumber == 1)
-                {
-                    transform.Translate((Vector3.back + Vector3.left) * dodgeSpeed * Time.deltaTime);
+                    if (randomNumber == 0)
+                    {
+                        transform.Translate((Vector3.back + Vector3.right) * dodgeSpeed * Time.deltaTime);
+                    }
+                    else if (randomNumber == 1)
+                    {
+                        transform.Translate((Vector3.back + Vector3.left) * dodgeSpeed * Time.deltaTime);
+                    }
                 }
             }
             else
             {
                 isDodgingBullet = false;
             }
+        }
+    }
+
+    // Check if enemy is in front of player or not
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "POV")
+        {
+            inBulletLine = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "POV")
+        {
+            inBulletLine = false;
         }
     }
 }
