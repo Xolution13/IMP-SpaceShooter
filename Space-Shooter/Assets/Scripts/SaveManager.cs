336e4fcd892ba7f67ca24ee719270b3bbce30588
Chirou <hc.stuehrk@gmail.com>
2018-02-21 13:04:33 +0100

    SaveSate

--- Space-Shooter/Assets/Scripts/SaveManager.cs
+++ Space-Shooter/Assets/Scripts/SaveManager.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { set; get; }
+    public SaveState state;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Save the whole state of this script to the player pref
+    public void Save()
+    {
+       // PlayerPrefs.SetString("save",);
+    }
+
+    // Load the previous saved state from player prefs
+    public void Load()
+    {
+        // Check if there is a save
+        if (PlayerPrefs.HasKey("save"))
+        {
+            //state = 
+        }
+        else
+        {
+
+        }
+    }
 }
