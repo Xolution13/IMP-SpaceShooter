2370e10c4d6eb72965df3779a9c738f439c0d857
Chirou <hc.stuehrk@gmail.com>
2018-02-21 13:00:43 +0100

    SaveManager

---
+++ Space-Shooter/Assets/Scripts/SaveState.cs
@@ -0,0 +1,10 @@
+﻿public class SaveState
+{
+    public int highScore = 0;
+
+    public float volumeSetting;
+    public float soundEffectSetting;
+
+    public bool useJoystick = false;
+    public bool useAccelerometer = true;
+}
