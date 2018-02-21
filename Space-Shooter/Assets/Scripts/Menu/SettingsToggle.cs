404357ccc9a77cc90feb28ab8538134d863001e1
Chirou <hc.stuehrk@gmail.com>
2018-02-21 16:54:37 +0100

    MenuSettings

---
+++ Space-Shooter/Assets/Scripts/Menu/SettingsToggle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class SettingsToggle : MonoBehaviour
+{
+    public GameObject ToggleAccelerometer;
+    public GameObject ToggleJoystick;
+
+    private void Start()
+    {
+        ToggleAccelerometer.GetComponent<Toggle>().on = false;
+        ToggleJoystick.GetComponent<Toggle>().on = true;
+    }
+}
