0ad45c39ded7e74916ba8f3452c0d8d7e0e8d82e
Chirou <hc.stuehrk@gmail.com>
2018-02-21 13:12:43 +0100

    Save

--- Space-Shooter/Assets/Scripts/Save/SaveDeserializer.cs
+++ Space-Shooter/Assets/Scripts/Save/SaveDeserializer.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.IO;
+using System.Xml.Serialization;
 
-public class SaveDeserializer : MonoBehaviour {
+public static class SaveDeserializer
+{
+    // Serialize
+    public static string Serialize<S>(this S toSerialize)
+    {
+        XmlSerializer xml = new XmlSerializer(typeof(S));
+        StringWriter writer = new StringWriter();
+        xml.Serialize(writer, toSerialize);
+        return writer.ToString();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // De-Serialize
+    public static S Deserialize<S>(this string toDeserialize)
+    {
+        XmlSerializer xml = new XmlSerializer(typeof(S));
+        StringReader reader = new StringReader(toDeserialize);
+        return (S)xml.Deserialize(reader);
+    }
 }
