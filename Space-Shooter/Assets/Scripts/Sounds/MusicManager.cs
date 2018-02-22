fa4cd898943fc53afb5611e0a490eef4eaa13aa2
Xolution13 <e-lewandowski@web.de>
2018-02-22 13:54:32 +0100

    MusicManager

---
+++ Space-Shooter/Assets/Scripts/Sounds/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Variables
+    private float volume;
+    private AudioSource musicSource;
+
+    private void Start()
+    {
+        volume = SaveManager.Instance.GetMusicVolume(volume);
+        musicSource = GetComponent<AudioSource>();
+
+        musicSource.volume = volume;
+    }
+}
