a13074507f3f6635b6722ecefb380c4cc30071a8
Xolution13 <e-lewandowski@web.de>
2018-02-22 14:22:03 +0100

    Music Script

---
+++ Space-Shooter/Assets/Scripts/Sounds/MenuMusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicManager : MonoBehaviour
+{
+    // Variables
+    private float volume;
+    private AudioSource musicSource;
+
+    // Get the saved volume of the background music and set it as volume
+    private void Start()
+    {
+        volume = SaveManager.Instance.GetMusicVolume(volume);
+        volume /= 100;
+        Debug.Log(volume);
+        musicSource = GetComponent<AudioSource>();
+
+        musicSource.volume = volume;
+    }
+
+    private void Update()
+    {
+        volume = SaveManager.Instance.GetMusicVolume(volume);
+        volume /= 100;
+        musicSource.volume = volume;
+    }
+}
