111eabd99eb7abea95942a8cf7f85e570b53fbde
Xolution13 <e-lewandowski@web.de>
2018-02-24 14:56:09 +0100

    Power-Up Spawner

--- Space-Shooter/Assets/Scripts/Power-Ups/PowerUpSpawner.cs
+++ Space-Shooter/Assets/Scripts/Power-Ups/PowerUpSpawner.cs
@@ -12,7 +12,7 @@ public class PowerUpSpawner : MonoBehaviour
     public GameObject shrinkPowerUp;
     public GameObject blackHolePowerUp;
     private List<GameObject> unlockedPowerUpList = new List<GameObject>();
-    private GameObject[] unlockedPowerUps;
+    public GameObject[] unlockedPowerUps;
 	public Vector3 spawnValues;
 	private float spawnWait;
 	public float spawnMostWait;
@@ -20,12 +20,16 @@ public class PowerUpSpawner : MonoBehaviour
 	public int startWait;
 	public bool stop;
 	private int randomPowerUP;
+    private bool noPowerUps = false;
 
-    // Start spawn-routine
+    // Check if any power ups are unlocked and only then start the spawner
 	private void Start ()
     {
         CheckForPowerUps();
-		StartCoroutine (waitSpawner ());
+        if (!noPowerUps)
+        {
+            StartCoroutine(waitSpawner());
+        }
 	}
 
 	private void Update ()
@@ -41,33 +45,42 @@ public class PowerUpSpawner : MonoBehaviour
             unlockedPowerUpList.Add(extraLifePowerUp);
         }
         // Check if the weapon power up is unlocked and add it to array
-        if (SaveManager.Instance.IsPowerUpUnlocked(1))
+        else if (SaveManager.Instance.IsPowerUpUnlocked(1))
         {
             unlockedPowerUpList.Add(weaponPowerUp);
         }
         // Check if the speed power up is unlocked and add it to array
-        if (SaveManager.Instance.IsPowerUpUnlocked(2))
+        else if (SaveManager.Instance.IsPowerUpUnlocked(2))
         {
             unlockedPowerUpList.Add(speedPowerUp);
         }
         // Check if the shield power up is unlocked and add it to array
-        if (SaveManager.Instance.IsPowerUpUnlocked(3))
+        else if (SaveManager.Instance.IsPowerUpUnlocked(3))
         {
             unlockedPowerUpList.Add(shieldPowerUp);
         }
         // Check if the shrink power up is unlocked and add it to array
-        if (SaveManager.Instance.IsPowerUpUnlocked(4))
+        else if (SaveManager.Instance.IsPowerUpUnlocked(4))
         {
             unlockedPowerUpList.Add(shrinkPowerUp);
         }
         // Check if the black-hole power up is unlocked and add it to array
-        if (SaveManager.Instance.IsPowerUpUnlocked(5))
+        else if (SaveManager.Instance.IsPowerUpUnlocked(5))
         {
             unlockedPowerUpList.Add(blackHolePowerUp);
+        }
+        else
+        {
+            Debug.Log("No power-ups unlocked!");
+            noPowerUps = true;
         }
 
         // Convert the list to the array
         unlockedPowerUps = unlockedPowerUpList.ToArray();
+        for (int i = 0; i < unlockedPowerUps.Length; i++)
+        {
+            Debug.Log("Spawning Power-Ups: " + unlockedPowerUps[i]);
+        }
     }
 
 	IEnumerator waitSpawner()
@@ -77,7 +90,7 @@ public class PowerUpSpawner : MonoBehaviour
         // Select random power-up and spawn it after random time (between min and max value) - wait if power-up has spawned
         while (true)
         {
-            randomPowerUP = Random.Range(0, unlockedPowerUps.Length);
+            randomPowerUP = Random.Range(0, unlockedPowerUps.Length + 1);
             Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0.5f, Random.Range(-spawnValues.z, spawnValues.z));
             if (!stop)
             {
