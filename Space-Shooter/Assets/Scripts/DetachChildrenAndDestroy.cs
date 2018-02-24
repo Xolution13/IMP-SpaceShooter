94ca7905e279852b4d5c57fd7c01a003620427dd
Xolution13 <e-lewandowski@web.de>
2018-02-24 18:41:30 +0100

    First Survival Level

---
+++ Space-Shooter/Assets/Scripts/DetachChildrenAndDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetachChildrenAndDestroy : MonoBehaviour
+{
+    // Add this to a prefab with children
+	private void Start ()
+    {
+        transform.DetachChildren();
+        Destroy(gameObject);
+	}
+}
--- Space-Shooter/Assets/Scripts/Survival/SurvivalLevel0.cs
+++ Space-Shooter/Assets/Scripts/Survival/SurvivalLevel0.cs
@@ -4,40 +4,59 @@ using UnityEngine;
 
 public class SurvivalLevel0 : MonoBehaviour
 {
+    // Variables
     private GameState stateScript;
     private GameScene sceneScript;
     private PlayerStatus status;
 
     public GameObject firstSpawn;
+    private int firstSpawnEnemyAmount;
     private bool firstSpawnOnce = false;
-    public GameObject secondSpawn;
 
-    private GameObject[] enemiesAlive;
+    public GameObject secondSpawn;
+    private float waitTime = 2f;
+    private bool secondSpawnOnce = false;
+    private int secondSpawnEnemyAmount;
+
+    private int totalEnemies;
 
     private void Start()
     {
         stateScript = GetComponent<GameState>();
         sceneScript = GetComponent<GameScene>();
         stateScript.gameTime = 30;
+
+        firstSpawnEnemyAmount = firstSpawn.transform.childCount;
+        secondSpawnEnemyAmount = secondSpawn.transform.childCount;
+        totalEnemies = firstSpawnEnemyAmount + secondSpawnEnemyAmount;
     }
 
     private void Update()
     {
+        // Spawn first wave after 2 seconds
         if (stateScript.gameTime <= 28 && !firstSpawnOnce)
         {
+            Debug.Log("Spawn 1");
             Instantiate(firstSpawn, transform.position, Quaternion.identity);
             firstSpawnOnce = true;
         }
 
-        enemiesAlive = GameObject.FindGameObjectsWithTag("Enemy");
-        for (int i = 0; i < enemiesAlive.Length; i++)
+        // Spawn second wave after all enemies of the first wave are destroyed
+        if (stateScript.destroyedEnemies == firstSpawnEnemyAmount && !secondSpawnOnce)
         {
-            if (!enemiesAlive[i])
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0)
             {
                 Instantiate(secondSpawn, transform.position, Quaternion.identity);
+                secondSpawnOnce = true;
             }
         }
-        
+
+        // Level is completed when all enemies are destroyed
+        if (stateScript.destroyedEnemies == totalEnemies)
+        {
+            sceneScript.CompleteLevel();
+        }
 
         // Level failed when time is up
         if (stateScript.gameTime <= 0)
