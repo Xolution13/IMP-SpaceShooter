c6b8b194e0763111e0282f905810a33e447ee001
Chirou <hc.stuehrk@gmail.com>
2018-02-28 13:18:46 +0100

    Virtual Joystick

--- Space-Shooter/Assets/Scripts/Player Controls/VirtualJoystick.cs
+++ Space-Shooter/Assets/Scripts/Player Controls/VirtualJoystick.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class VirtualJoystick : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image joystickImg;
+
+    public Vector3 InputDirection { set; get; }
+
+    private void Start()
+    {
+        joystickImg = GetComponent<Image>();
+        InputDirection = Vector3.zero;
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos = Vector2.zero;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle
+            (joystickImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / joystickImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / joystickImg.rectTransform.sizeDelta.y);
+
+            float x = (joystickImg.rectTransform.pivot.x == 1) ? pos.x * 2 + 1 : pos.x * 2 - 1;
+            float y = (joystickImg.rectTransform.pivot.y == 1) ? pos.y * 2 + 1 : pos.y * 2 - 1;
+
+            InputDirection = new Vector3(x, y, 0);
+            InputDirection = (InputDirection.magnitude > 1) ? InputDirection.normalized : InputDirection;
+
+            Debug.Log(InputDirection);
+        }
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        InputDirection = Vector3.zero;
+    }
 }
