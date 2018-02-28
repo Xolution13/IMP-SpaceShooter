3ed13b29b10053efc52af139b24a0fccb99e1062
Chirou <hc.stuehrk@gmail.com>
2018-02-28 16:02:38 +0100

    MovementChanges

--- Space-Shooter/Assets/Scripts/Player Controls/AccelerometerMovement.cs
+++ Space-Shooter/Assets/Scripts/Player Controls/AccelerometerMovement.cs
@@ -25,7 +25,7 @@ public class AccelerometerMovement : MonoBehaviour
         status = GetComponent<PlayerStatus>();
         if (!inMenuScene)
         {
-            moveJoystick = FindObjectOfType<VirtualJoystick>();
+            moveJoystick = FindObjectOfType<VirtualJoystick>().GetComponent<VirtualJoystick>();
             joystickImage = FindObjectOfType<VirtualJoystick>().GetComponent<Image>();
         }
     }
@@ -51,7 +51,7 @@ public class AccelerometerMovement : MonoBehaviour
                     {
                         joystickImage.enabled = true;
                         _InputDir = moveJoystick.InputDirection;
-                        transform.Translate(new Vector3(((_InputDir.x) * Time.deltaTime * movementSpeed), 0.0f, ((_InputDir.y) * Time.deltaTime * movementSpeed)));
+                        transform.Translate(new Vector3(((_InputDir.x) * Time.deltaTime * (movementSpeed / 2)), 0.0f, ((_InputDir.y) * Time.deltaTime * (movementSpeed / 2))));
                         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -23.5f, 23.5f), 0.5f, Mathf.Clamp(transform.position.z, -16, 16));
                     }
                 }
